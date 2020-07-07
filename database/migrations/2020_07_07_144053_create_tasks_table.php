b4c75592079964a85ff8003d6379203fce7a9a3b
v.bazhenov <sleem81@list.ru>
2020-07-07 22:22:35 +0500

    migation Task

--- database/migrations/2020_07_07_144053_create_tasks_table.php
+++ database/migrations/2020_07_07_144053_create_tasks_table.php
@@ -16,6 +16,8 @@ class CreateTasksTable extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
+            $table->string('name');
+            $table->text('body')->require();
         });
     }
 
